b7b52533d0a19e9fd1a3449fd061e64a865db98e
khushi kandoi <khushikandoii09@gmail.com>
2023-09-10 14:08:56 +0530

    updated spinner

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './Home'
 import NotifyAll from './NotifyAll'
+import Spinner from './Spinner'
 
 const App = () => {
   return (
     <>
-      <Router>
+      {/* <Router>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/notifyAll" element={<NotifyAll/>} />
         </Routes>
-      </Router>
-
+      </Router> */}
+      <Spinner/>
     </>
   )
 }

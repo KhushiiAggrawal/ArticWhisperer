8c64560bd140a119cae50c5ac09d6dee0d355cfb
khushi kandoi <khushikandoii09@gmail.com>
2023-09-10 13:12:55 +0530

    updated with routes

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Spinner from './Spinner'
 import { useState } from 'react';
+import axios from "axios"
 
 
 const Home = () => {
+    const [loading, setLoading] = useState(false);
+    const [id, setId] = useState("");
+    const [notiMsg, setNotiMsg] = useState("");
+
+    const notify = async()=>{
+        try { 
+            setLoading(true); // if u implement loading thingie
+            const { data } = await axios.post(`https://articmailserver.onrender.com/notifications/notify`, 
+              { id , notiMsg }, 
+              { 
+                headers: { 
+                  "Content-Type": "application/json", 
+                }, 
+                withCredentials: true, 
+              } 
+            ); 
+            console.log(data.message);// your response from server 
+          } catch (error) { 
+            if (error.response.data) { 
+              console.log(error.response.data.message); 
+            } 
+            setLoading(false); 
+          }
+    }
 
     return (
         <>
             <div className='wrapper'>
-                <div className='content'>
+                <form className='content'>
                     <img src="/icon/Logo.png" alt="logo" className="logo" />
                     <h2>Send Notification</h2>
                     <div className="innerDiv">
@@ -19,6 +44,8 @@ const Home = () => {
                                 type="text"
                                 placeholder="User id"
                                 required
+                                value={id}
+                                onChange={(e) => setId(e.target.value)}
                             ></input>
                         </div>
                         <div className="inputdiv">
@@ -27,15 +54,19 @@ const Home = () => {
                                 type="text"
                                 placeholder="Type your message here...."
                                 required
+                                value={notiMsg}
+                                onChange={(e) => setNotiMsg(e.target.value)}
                             ></textarea>
                         </div>
                         <Link to="/notifyAll" className='a'>Notify all ?</Link>
-                        <button>
+                        <button onClick={notify}>
                             Send Notification
                         </button>
                     </div>
-                </div>
+                </form>
+                {loading ? <Spinner /> : null}
             </div>
+            
         </>
     )
 }
